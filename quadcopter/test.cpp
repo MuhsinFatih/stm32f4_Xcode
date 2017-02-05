b7373567ab3335cab28f3ab81a88e5cdc1ef5343
Mehmet Emin Kartal <kartalbombe@icloud.com>
2017-02-06 00:34:06 +0300

    cpp working kindof

---
+++ quadcopter/test.cpp
@@ -0,0 +1,10 @@
+//
+//  test.cpp
+//  quadcopter
+//
+//  Created by Mehmet emin Kartal on 06/02/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#include "test.hpp"
+
---
+++ quadcopter/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  quadcopter
+//
+//  Created by Mehmet emin Kartal on 06/02/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
