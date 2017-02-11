30026aa057dfc2ab45838cb30601bc6981592ca7
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2017-02-11 20:19:35 +0300

    main.cpp

---
+++ quadcopter/main.cpp
@@ -0,0 +1,13 @@
+//
+//  test.cpp
+//  quadcopter
+//
+//  Created by Mehmet emin Kartal on 06/02/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#include "main.hpp"
+
+int main() {
+	
+}
---
+++ quadcopter/main.hpp
@@ -0,0 +1,46 @@
+//
+//  test.hpp
+//  quadcopter
+//
+//  Created by Mehmet emin Kartal on 06/02/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stm32f4xx.h>
+
+
+#define OUTPUT	GPIO_Mode_OUT
+#define INPUT	GPIO_Mode_IN
+#define NOPULL	GPIO_PuPd_NOPULL
+
+#define true	1
+#define false	0
+
+#define read(idr,pin) (idr & pin)
+#define enableFloatingPoint() (*((int*)0xE000ED88))|=0x0F00000;  // Floating Point donanimini aktiflestir.
+#define REP(size) for(size_t i=0, length=size; i<length; ++i)
+#define REPW(size)  size_t w,length; length=size; while(w<length)
+
+#ifndef BAUDRATE
+#define BAUDRATE 230400
+#endif
+
+
+
+#ifdef __cplusplus
+
+extern "C" {
+#include "def.h"
+#include "usart.hpp"
+#endif
+	int main();
+	void cpp_main();
+#ifdef __cplusplus
+}
+#endif
+
