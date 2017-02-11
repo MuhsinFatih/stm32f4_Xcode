98b30220d98cbd3e1c911201f9593484854d397e
Muhsin Fatih Yorulmaz <muhsinfatih@gmail.com>
2017-02-11 17:22:28 +0300

    commit

---
+++ quadcopter/usart.hpp
@@ -0,0 +1,19 @@
+//
+//  usart.hpp
+//  quadcopter
+//
+//  Created by Muhsin Fatih Yorulmaz on 11/02/2017.
+//  Copyright © 2017 mekatrotekno. All rights reserved.
+//
+
+#ifndef usart_hpp
+#define usart_hpp
+
+#include <stdio.h>
+
+class usart {
+	usart(int rx, int tx);
+	
+};
+
+#endif /* usart_hpp */
